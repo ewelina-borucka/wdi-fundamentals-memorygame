b8810871e65a4cd0a50e177a67a17d20036c14b7
Ewelina Borucka <ewelina.borucka@gmail.com>
2016-10-22 16:09:40 +1100

    unit 10 for loop to create divs (cards)

--- starter-code/main.js
+++ starter-code/main.js
@@ -15,3 +15,24 @@ else {
 }
 
 
+
+var board = document.getElementById("game-board");
+
+function createCards() {
+	for (var i = 0; i < 4; i = i + 1) {
+
+		// create a div element, it will be used as a card
+		var newCards = document.createElement("div");
+
+		// add a class to the card element 
+		newCards.className = "card";
+
+		// append the cards to the gameboard 
+		board.appendChild(newCards);
+	}
+}
+
+createCards();
+
+
+
